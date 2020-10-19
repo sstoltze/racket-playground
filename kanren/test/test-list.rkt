68454eb82de36e9aaae8cbc90a0cad2f0d31c72e
sstoltze <sstoltze@gmail.com>
2020-10-19 22:23:47 +0200

    Reorganize test files

---
+++ kanren/test/test-list.rkt
@@ -0,0 +1,29 @@
+#lang racket/base
+
+(require "kanren.rkt")
+
+(define member-test
+  (run* (q)
+        (membero q (list 1 2 3))
+        (membero q (list 3 4 5 6))))
+
+(define member-test-2
+  (run* (q)
+        (membero q (list 1 2 3))
+        (membero q (list 4 5 6))))
+
+(define append-test
+  (run* (q p)
+        (appendo p q '(1 2 3))))
+
+(define second-append-test
+  (run* (q)
+        (fresh (x y)
+               (appendo x y '(1 2 3 4 5))
+               (congruent q (list x y)))))
+
+(define list-test
+  (run* (q p b)
+        (congruent q p)
+        (congruent b 5)
+        (congruent p (list 1 2 3 b))))
---
+++ kanren/test/test-programs.rkt
@@ -0,0 +1,50 @@
+#lang racket/base
+
+(require "kanren.rkt")
+
+(define a-and-b
+  (call/empty-state
+   (conj
+    (call/fresh (lambda (a) (congruent a 7)))
+    (call/fresh (lambda (b) (disj
+                             (congruent b 5)
+                             (congruent b 6)))))))
+
+(define many-vars
+  (call/empty-state
+   (call/fresh
+    (lambda (q)
+      (call/fresh
+       (lambda (p)
+         (conj
+          (congruent q p)
+          (congruent p 5))))))))
+
+(define many-vars-fresh
+  (call/empty-state
+   (fresh (p q)
+          (conj
+           (congruent p q)
+           (congruent p 5)))))
+
+(define impossible-many-vars
+  (call/empty-state
+   (fresh (q p)
+          (conj
+           (conj
+            (congruent q p)
+            (congruent p 5))
+           (congruent q 4)))))
+
+(define run-test
+  (run* (q x y)
+        (appendo x y '(1 2 3 4 5))
+        (congruent q (list x y))))
+
+(define run-all-test
+  (run/all* (x y)
+            (appendo x y '(1 2 3 4 5))))
+
+(define reify-test
+  (run* (x y)
+        (congruent x y)))
